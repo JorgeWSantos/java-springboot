5d3491153207013588fc015eb24532b8a8227642
JorgeWS <jorgews.dev@gmail.com>
2023-10-11 19:00:27 -0300

    feat: 🎸 Iniciará projeto java todolist

---
+++ src/test/java/br/com/jorgesantos/todolist/TodolistApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.jorgesantos.todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodolistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
